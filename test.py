1ba5b09d06f3844163d387ff2e640bde6a290593
rfnshare <rfnshare@gmail.com>
2021-11-18 14:12:29 +0600

    Signup test case complete except google button

--- test.py
+++ test.py
@@ -11,21 +11,21 @@ driver.get('https://www.pinterest.com/')
 
 #  ------------------------------------- Log In TC_002 -------------------------------------- #
 
-# TC_2.1 ----------------- #
+# TC_2.1 Login by email----------------- #
 # login = driver.find_element_by_xpath('//div[@class="tBJ dyH iFc yTZ erh tg7 mWe"]')
 # login.click()
 # driver.find_element_by_xpath("//input[@id='email']").send_keys('demo@gmail.com')
 # driver.find_element_by_xpath("//input[@id='password']").send_keys('123456')
 # time.sleep(5)
 
-# TC_2.2 ----------------- #
+# TC_2.2 Login by Facebook----------------- #
 # login = driver.find_element_by_xpath('//div[@class="tBJ dyH iFc yTZ erh tg7 mWe"]')
 # login.click()
 # login_fb = driver.find_element_by_xpath("//div[@data-test-id='facebook-connect-button']")
 # login_fb.click()
 # time.sleep(5)
 
-# TC_2.3 ----------------- #
+# TC_2.3 Login by Google----------------- # Problem***
 # login = driver.find_element_by_xpath('//div[@class="tBJ dyH iFc yTZ erh tg7 mWe"]')
 # login.click()
 # login_google = driver.find_element_by_xpath("//div[@data-test-id='google-connect-button']")
@@ -33,4 +33,42 @@ driver.get('https://www.pinterest.com/')
 # time.sleep(5)
 
 
+#  ------------------------------------- Sign Up TC_003 -------------------------------------- #
+# TC_3.1 Signup with email----------------- #
+# sign_up = driver.find_element_by_xpath(
+#     '//div[@class="tBJ dyH iFc yTZ pBj tg7 mWe"]')
+# sign_up.click()
+# email_field = driver.find_element_by_xpath("//input[@id='email']")
+# time.sleep(3)
+# email_field.send_keys("demo@gmail.com")
+# time.sleep(3)
+# pass_filed = driver.find_element_by_xpath("//input[@id='password']")
+# time.sleep(3)
+# pass_filed.send_keys("123456")
+# time.sleep(3)
+# # age_filed = driver.find_element_by_xpath("//input[@id='age']")
+# # time.sleep(3)
+# # age_filed.send_keys("25")
+# # time.sleep(3)
+# driver.find_element_by_xpath("//div[text()='Continue']").click()
+# time.sleep(5)
+# driver.quit()
+
+# TC_3.2 Signup by Facebook----------------- #
+# login = driver.find_element_by_xpath('//div[@class="tBJ dyH iFc yTZ erh tg7 mWe"]')
+# login.click()
+# login_fb = driver.find_element_by_xpath("//div[@data-test-id='facebook-connect-button']")
+# login_fb.click()
+# time.sleep(5)
+
+# TC_2.3 Signup by Google----------------- # Problem***
+# login = driver.find_element_by_xpath('//div[@class="tBJ dyH iFc yTZ erh tg7 mWe"]')
+# login.click()
+# # login_google = driver.find_element_by_xpath("//div[@class='zI7 iyn Hsu']")
+# # print(login_google.text)
+# wait = WebDriverWait(driver, 10)
+# wait.until(EC.presence_of_element_located((By.XPATH, "//div[@data-test-id='google-connect-button']")))
+# a = driver.find_element_by_xpath("//div[@data-test-id='google-connect-button']")
+# a.click()
+# time.sleep(5)
 
