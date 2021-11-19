6951b1a87255e195f0703fb0a3850f386713856f
rfnshare <rfnshare@gmail.com>
2021-11-19 10:43:05 +0600

    login by email & fb

--- test.py
+++ test.py
@@ -12,11 +12,12 @@ driver.get('https://www.pinterest.com/')
 #  ------------------------------------- Log In TC_002 -------------------------------------- #
 
 # TC_2.1 Login by email----------------- #
-# login = driver.find_element_by_xpath('//div[@class="tBJ dyH iFc yTZ erh tg7 mWe"]')
-# login.click()
-# driver.find_element_by_xpath("//input[@id='email']").send_keys('demo@gmail.com')
-# driver.find_element_by_xpath("//input[@id='password']").send_keys('123456')
-# time.sleep(5)
+login = driver.find_element_by_xpath('//div[@class="tBJ dyH iFc yTZ erh tg7 mWe"]')
+login.click()
+driver.find_element_by_xpath("//input[@id='email']").send_keys('demo@gmail.com')
+driver.find_element_by_xpath("//input[@id='password']").send_keys('123456')
+time.sleep(5)
+driver.find_element_by_xpath("//div[@data-test-id='registerFormSubmitButton']").click()
 
 # TC_2.2 Login by Facebook----------------- #
 # login = driver.find_element_by_xpath('//div[@class="tBJ dyH iFc yTZ erh tg7 mWe"]')
@@ -40,26 +41,38 @@ driver.get('https://www.pinterest.com/')
 # sign_up.click()
 # email_field = driver.find_element_by_xpath("//input[@id='email']")
 # time.sleep(3)
-# email_field.send_keys("demo@gmail.com")
+# email_field.send_keys("af.qups@gmail.com")
 # time.sleep(3)
 # pass_filed = driver.find_element_by_xpath("//input[@id='password']")
 # time.sleep(3)
-# pass_filed.send_keys("123456")
+# pass_filed.send_keys("asdfgh.123")
 # time.sleep(3)
-# # age_filed = driver.find_element_by_xpath("//input[@id='age']")
-# # time.sleep(3)
-# # age_filed.send_keys("25")
-# # time.sleep(3)
+# age_filed = driver.find_element_by_xpath("//input[@id='age']")
+# time.sleep(3)
+# age_filed.send_keys("25")
+# time.sleep(3)
 # driver.find_element_by_xpath("//div[text()='Continue']").click()
 # time.sleep(5)
 # driver.quit()
 
 # TC_3.2 Signup by Facebook----------------- #
+# main_page = driver.current_window_handle
 # login = driver.find_element_by_xpath('//div[@class="tBJ dyH iFc yTZ erh tg7 mWe"]')
 # login.click()
 # login_fb = driver.find_element_by_xpath("//div[@data-test-id='facebook-connect-button']")
 # login_fb.click()
 # time.sleep(5)
+# for handle in driver.window_handles:
+#     if handle != main_page:
+#         fb_login = handle
+# driver.switch_to.window(fb_login)
+# fb_email = driver.find_element_by_xpath("//input[@id='email']")
+# fb_email.send_keys("af.qups@gmail.com")
+# fb_pass = driver.find_element_by_xpath("//input[@id='pass']")
+# fb_pass.send_keys("asdfgh123")
+# fb_submit = driver.find_element_by_xpath("//input[@name='login']")
+# fb_submit.click()
+
 
 # TC_2.3 Signup by Google----------------- # Problem***
 # login = driver.find_element_by_xpath('//div[@class="tBJ dyH iFc yTZ erh tg7 mWe"]')
@@ -72,3 +85,7 @@ driver.get('https://www.pinterest.com/')
 # a.click()
 # time.sleep(5)
 
+#  ------------------------------------- Homepage TC_004 -------------------------------------- #
+# TC_4.1 LOGO----------------- #
+# logo = driver.find_element_by_xpath("//div[@data-test-id='pinterest-logo']")
+# logo.click()
